c471baff6d2fbbb5f14f2a324a0dee9f149009bc
Rima Chhaya <rims29@hotmail.com>
2025-09-09 20:34:43 -0400

    added coinflip

--- src/index.js
+++ src/index.js
@@ -8,7 +8,55 @@ let greeting = (name) => {
 }
 
 
+ let flipacoin = (num) => {
+ let options = ['heads','tails'];
+ let heads = 0;
+ let tails = 0;
+ for (let i = 0; i < num; i++ ) {
+  let flip = options[Math.floor(Math.random ()* 2)]
+  if (flip === 'heads') {
+    heads++
+  } else {
+    tails++
+  }
+ }
+ alert (`you fliped ${heads} heads and ${tails} tails`)
+  }
+
+
+ let playrps = () => {
+  let options = ['r','p','s'];
+  let computerinput = options [Math.floor(Math.random ()* 3)]
+  let userinput = prompt ('please enter r,p,s')
+  if (userinput !== 'r' && userinput !== 'p' && userinput !== 's') {
+    return;
+  }
+  if (userinput === computerinput) {
+  alert (`you picked ${userinput} && computerpicked ${computerinput} you tied`)
+  } else if (userinput == 'r' && computerinput == 's') {
+    alert (`you picked ${userinput} && computer picked ${computerinput} you win`)
+  } else if (userinput == 's' && computerinput == 'p') {
+    alert (`you picked ${userinput} && computer picked ${computerinput} you win`)
+  } else if (userinput == 'p' && computerinput == 'r') {
+    alert (`you picked ${userinput} && computer picked ${computerinput} you win`)
+  } else {
+    alert (`you picked ${userinput} && computer picked ${computerinput} you lose`)
+  }
+
+  }
+
+  let add = (a, b) => a + b;
+
 document.addEventListener('DOMContentLoaded', () => {
     showOutput('DOM fully loaded and parsed');
     showOutput(greeting('world'))
+    // flipacoin(10)
+    // playrps()
+    // Add more function calls here as needed
+
+    showOutput(add(2, 3)); // Example usage of add function
+    console.log('Add function result:', add(2, 3));
+    console.log('something else')
+
+
 });
